47095ba97d596070748c2173d1c240b30c72cfdb
lovewhoilove <3528300287@qq.com>
2021-12-11 23:42:29 +0800

    自定义测量

--- src/components/Maptools.vue
+++ src/components/Maptools.vue
@@ -57,6 +57,12 @@ export default {
                 case 'area':
                     this.initAreaMap();
                     break;
+                case 'diymeasurement_distance':
+                    this.initDIYMeasurement('distance');
+                    break;
+                case 'diymeasurement_area':
+                    this.initDIYMeasurement('area');
+                    break;
                 case 'spacequery':
                     this.initSpaceQuery();
                     break;
@@ -73,6 +79,155 @@ export default {
                     break;
                 default:
                     break;
+            }
+        },
+        //地图距离量算
+        async initDistanceMap() {
+            console.log('距离测量');
+            const _self = this;
+            const view = _self.$store.getters._getDefaultMapView;
+            const [DistanceMeasurement2D] = await loadModules(['esri/widgets/DistanceMeasurement2D'], config.options);
+            if (this.measurementWidget) this.measurementWidget.destroy();
+            this.measurementWidget = new DistanceMeasurement2D({
+                view: view,
+            });
+            view.ui.add(this.measurementWidget, 'top-left');
+        },
+        //地图面积量算
+        async initAreaMap() {
+            console.log('面积测量');
+            const _self = this;
+            const view = _self.$store.getters._getDefaultMapView;
+            const [AreaMeasurement2D] = await loadModules(['esri/widgets/AreaMeasurement2D'], config.options);
+            if (this.measurementWidget) this.measurementWidget.destroy();
+            this.measurementWidget = new AreaMeasurement2D({
+                view: view,
+            });
+            view.ui.add(this.measurementWidget, 'top-left');
+        },
+        //自定义测量
+        async initDIYMeasurement(type) {
+            const _self = this;
+            const view = _self.$store.getters._getDefaultMapView;
+
+            const resultLayer = view.map.findLayerById('measurementGraphicLayer');
+            if (resultLayer) view.map.remove(resultLayer);
+            const [
+                SketchViewModel,
+                Graphic,
+                GraphicsLayer,
+                GeometryService,
+                LengthsParameters,
+                AreasAndLengthsParameters,
+            ] = await loadModules(
+                [
+                    'esri/widgets/Sketch/SketchViewModel',
+                    'esri/Graphic',
+                    'esri/layers/GraphicsLayer',
+                    'esri/tasks/GeometryService',
+                    'esri/tasks/support/LengthsParameters',
+                    'esri/tasks/support/AreasAndLengthsParameters',
+                ],
+                config.options,
+            );
+
+            const graphicsLayer = new GraphicsLayer({
+                id: 'measurementGraphicLayer',
+                elevationInfo: {
+                    mode: 'on-the-ground',
+                },
+            });
+            view.map.add(graphicsLayer);
+
+            if (type === 'distance') {
+                const polylineSymbol = {
+                    type: 'simple-line',
+                    color: '#d81e06',
+                    width: '2',
+                    style: 'solid',
+                };
+
+                const sketchViewModel = new SketchViewModel({
+                    updateOnGraphicClick: false,
+                    view: view,
+                    layer: graphicsLayer,
+                    polylineSymbol,
+                });
+                sketchViewModel.create('polyline');
+
+                sketchViewModel.on('create-complete', function (event) {
+                    const graphic = new Graphic({
+                        geometry: event.geometry,
+                        symbol: sketchViewModel.graphic.symbol,
+                    });
+                    graphicsLayer.add(graphic);
+                });
+
+                sketchViewModel.on('create', function (event) {
+                    if (event.state === 'complete') {
+                        //获取线段长度
+                        const geometryService = new GeometryService({
+                            url: 'http://sampleserver6.arcgisonline.com/arcgis/rest/services/Utilities/Geometry/GeometryServer',
+                        });
+
+                        const lengthsParameters = new LengthsParameters();
+                        lengthsParameters.polylines = event.graphic.geometry;
+                        //9036：千米；9001：米
+                        lengthsParameters.lengthUnit = 9036;
+                        //如果多段线位于地理坐标系中，则需要将测地线设置为TRUE，
+                        //以便计算多段线中每对顶点之间的椭球最短路径距离。如果未指定length thUnit，则返回以米为单位的输出。
+                        lengthsParameters.geodesic = true;
+
+                        geometryService.lengths(lengthsParameters).then(function (result) {
+                            console.log('长度：', result.lengths[0] + '千米');
+                        });
+                    }
+                });
+            } else if (type === 'area') {
+                const polygonSymbol = {
+                    type: 'simple-fill',
+                    color: 'rgba(216,30,6, 0.4)',
+                    style: 'solid',
+                    outline: {
+                        color: '#d81e06',
+                        width: 1,
+                    },
+                };
+
+                const sketchViewModel = new SketchViewModel({
+                    updateOnGraphicClick: false,
+                    view: view,
+                    layer: graphicsLayer,
+                    polygonSymbol,
+                });
+
+                sketchViewModel.create('polygon');
+                sketchViewModel.on('create-complete', function (event) {
+                    const graphic = new Graphic({
+                        geometry: event.geometry,
+                        symbol: sketchViewModel.graphic.symbol,
+                    });
+                    graphicsLayer.add(graphic);
+                });
+
+                sketchViewModel.on('create', function (event) {
+                    if (event.state === 'complete') {
+                        //获取周长和面积
+                        const geometryService = new GeometryService({
+                            url: 'http://sampleserver6.arcgisonline.com/arcgis/rest/services/Utilities/Geometry/GeometryServer',
+                        });
+
+                        const areasAndLengthsParameters = new AreasAndLengthsParameters();
+                        areasAndLengthsParameters.polygons = event.graphic.geometry;
+                        areasAndLengthsParameters.areaUnit = 'square-kilometers';
+                        areasAndLengthsParameters.lengthUnit = 'kilometers';
+
+                        geometryService.areasAndLengths(areasAndLengthsParameters).then(function (result) {
+                            console.log('面积：', result.areas[0] + '平方千米');
+                            console.log('周长', result.lengths[0] + '千米');
+                        });
+                    }
+                });
             }
         },
         //初始化空间查询
@@ -346,30 +501,6 @@ export default {
         openMaptreePannel() {
             let currentVisible = this.$store.getters._getDefaultMaptreeVisible;
             this.$store.commit('_setDefaultMaptreeVisible', !currentVisible);
-        },
-        //地图距离量算
-        async initDistanceMap() {
-            console.log('距离测量');
-            const _self = this;
-            const view = _self.$store.getters._getDefaultMapView;
-            const [DistanceMeasurement2D] = await loadModules(['esri/widgets/DistanceMeasurement2D'], config.options);
-            if (this.measurementWidget) this.measurementWidget.destroy();
-            this.measurementWidget = new DistanceMeasurement2D({
-                view: view,
-            });
-            view.ui.add(this.measurementWidget, 'top-left');
-        },
-        //地图面积量算
-        async initAreaMap() {
-            console.log('面积测量');
-            const _self = this;
-            const view = _self.$store.getters._getDefaultMapView;
-            const [AreaMeasurement2D] = await loadModules(['esri/widgets/AreaMeasurement2D'], config.options);
-            if (this.measurementWidget) this.measurementWidget.destroy();
-            this.measurementWidget = new AreaMeasurement2D({
-                view: view,
-            });
-            view.ui.add(this.measurementWidget, 'top-left');
         },
     },
 };
